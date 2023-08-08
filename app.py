392fd4f7d40d3fec5da2bf96e7f055e278042f7c
Evergyu <iingy123@naver.com>
2023-08-08 20:47:15 +0900

    Refactoring

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def get_frame():
     global camera  # 전역 변수 사용
 
     if camera is None:
-        camera = cv2.VideoCapture(0)
+        camera = cv2.VideoCapture(0) #첫번째 카메라를 객체로 가져옴
 
     while True:
         success, frame = camera.read()
@@ -21,7 +21,7 @@ def get_frame():
             break
         else:
             # 프레임을 JPEG 이미지로 인코딩하여 반환
-            ret, buffer = cv2.imencode('.jpg', frame)
+            ret, buffer = cv2.imencode('.jpg', frame) #카메라로부터 현재의 영상 읽어오기
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
@@ -38,31 +38,23 @@ def index():
 def video_feed():
     return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
 @app.route('/capture')
 def capture():
-    global capture_start_time, camera  # 전역 변수 사용
+    global camera  # 전역 변수 사용
 
-    if capture_start_time is None:
-        capture_start_time = time.time()
+    success, frame = camera.read()
 
-    time_left = 3 - (time.time() - capture_start_time)
-    if time_left > 0:
-        return f'캡처 대기 중... {int(time_left)}초 남음'
+    if success:
+        image_name = f'captured_image.jpg'
+        # 이미지 저장
+        cv2.imwrite(os.path.join(image_name), frame)
+        capture_start_time = None  # 캡처 완료 후 타이머 초기화
+        print(f"이미지 성공적으로 저장. ({image_name})")
+        return f'이미지 성공적으로 저장. ({image_name})'
     else:
-        success, frame = camera.read()
-
-        if success:
-            count = len(os.listdir('captured_images')) + 1
-            image_name = f'captured_image_{count}.jpg'
-            # 이미지 저장
-            cv2.imwrite(os.path.join('captured_images', image_name), frame)
-            capture_start_time = None  # 캡처 완료 후 타이머 초기화
-            return f'이미지 성공적으로 저장. ({image_name})'
-        else:
-            capture_start_time = None  # 캡처 실패 후 타이머 초기화
-            return '이미지 저장 실패.'
-
+        capture_start_time = None  # 캡처 실패 후 타이머 초기화
+        print("이미지 저장 실패.")
+        return '이미지 저장 실패.'
 
 if __name__ == '__main__':
     app.run(debug=True)
